59a0aaeb127e1319c7f04d98da8b2eecfafaecb7
tsgouros <tsgouros@gmail.com>
2018-05-23 16:54:57 -0500

    separated viz catalog from draw dialog

--- js/src/AMSControlPanel/index.js
+++ js/src/AMSControlPanel/index.js
@@ -28,7 +28,7 @@ import NumberSliderWidget from 'paraviewweb/src/React/Widgets/NumberSliderWidget
 //
 // Input data includes: vizCatalog, dataCatalog.  See js/src/index.js for
 // description of these objects.  There is also a deliverDrawCommand
-// function sent down for sending the output back up.  
+// function sent down for sending the output back up.
 //
 // State includes the input vizCatalog and dataCatalog.  The vizCatalog is
 // subject to modification by the vizDialog.
@@ -59,18 +59,26 @@ class AMSControlPanel extends React.Component {
     // The dialog specs are built from the client state, to show the user a
     // menu of options.
     this.drawDialogSpec = {};
-    this.vizDialogSpec = {};
-    this.currentViz = Object.keys(props.vizCatalog)[0];
     this.currentData = Object.keys(props.dataCatalog)[0];
-    
-    this.returnDrawCommand = this.returnDrawCommand.bind(this);
-    this.returnVizCatalogEntry = this.returnVizCatalogEntry.bind(this);
 
-    this.buildVizDialogSpecs();
+    this.returnDrawCommand = this.returnDrawCommand.bind(this);
+
     this.buildDrawDialogSpecs();
   }
 
+
+    // Adjust the corresponding entry in the drawDialogSpec.  First we
+    // adjust the list of data sets to correspond to the data catalog (in
+    // case it has changed recently).
+    // this.drawDialogSpec.plotName.ui.domain =
+    //   Object.keys(this.props.dataCatalog).reduce(function(res, cur) {
+    //     res[cur] = cur;
+    //     return res;
+    //   }, {});
+
+
   buildDrawDialogSpecs() {
+    this.currentViz = Object.keys(this.props.vizCatalog)[0];
     this.drawDialogSpec = {
       plotName: {
         data: { value: this.currentViz, id: "plotName" },
@@ -127,226 +135,6 @@ class AMSControlPanel extends React.Component {
         this.drawDialogSpec[key].onChange.bind(this);
     };
   }
-    
-  buildVizDialogSpecs() {
-    
-    this.vizDialogSpec = {
-      CellPlotName: {
-        data: { value: [ this.currentViz ], id: "CellPlotName" },
-        widgetType: "cell",
-        depth: 1,
-        ui: {
-          propType: "cell",
-          label: "Visualization name",
-          domain: { range: [{ force: false }] },
-          type: "string",
-          layout: '1',
-          help: "Give this collection of plot parameters a name so you can use it again.",
-          componentLabels: [''],
-        },
-        show: () => true,        
-        onChange: function onChange(data) {
-          console.log("changing the plot name:", data);
-          this.vizDialogSpec[data.id].data.value = data.value;
-          this.currentViz = data.value[0];
-          this.render();
-        }
-      },
-      EnumPlotType: {
-        data: { value: "contour", id: "EnumPlotType" },
-        widgetType: "enum",
-        depth: 4,
-        ui: {
-          propType: "enum",
-          label: "Visualization type",
-          domain: ["contour", "streamlines"].reduce(function(res, cur) {
-            res[cur] = cur;
-            return res;
-          }, {}),  // the list of possible values
-          type: "string",           // 'string' or 'int'
-          layout: '1',
-          help: "Choose the type of plot to view.",
-          componentLabels: [''],
-        },
-        show: () => true,
-        onChange: function onChange(data) {
-          this.vizDialogSpec[data.id].data.value = data.value[0];
-          data.value = this.vizDialogSpec[data.id].data.value;
-          this.render();
-        }
-      },
-      EnumContourVariable: {
-        data: { value: "uds_0_scalar", id: "EnumContourVariable" },
-        widgetType: "enum",
-        depth: 8,
-        ui: {
-          propType: "enum",
-          label: "Contour variable",
-          domain: ["uds_0_scalar",
-                   "pressure",
-                   "axial_velocity",
-                   "radial_velocity",
-                   "tangential_velocity"
-                  ].reduce(function(res, cur) {
-            res[cur] = cur;
-            return res;
-          }, {}),
-          type: "string",
-          layout: '1',
-          help: "Which value to contour?",
-          componentLabels: [''],
-        },
-        show: () => {
-          if (this.vizDialogSpec.EnumPlotType.data.value === "contour") {
-            return true;
-          } else {
-            return false;
-          }
-        },
-        onChange: function onChange(data) {
-          this.vizDialogSpec[data.id].data.value = data.value[0];
-          data.value = this.vizDialogSpec[data.id].data.value;
-          this.render();
-        }
-      },
-      DoubleContourValue: {
-        data: {value: [400.0], id: "DoubleContourValue" },
-        widgetType: "cell",
-        depth: 12,
-        ui: {
-          propType: "cell",
-          label: "Contour value",
-          domain: { range: [{ min: 0.0, max: 800.0, force: true }] },
-          type: "double",
-          layout: '1',
-          help: "Select a contour value",
-          componentLabels: [''],
-        },
-        show: () => {
-          if (this.vizDialogSpec.EnumPlotType.data.value === "contour") {
-            return true;
-          } else {
-            return false;
-          }
-        },
-        onChange: function onChange(data) {
-          this.vizDialogSpec[data.id].data.value = data.value;
-          this.render();
-        }
-      },
-      EnumColorVariable: {
-        data: { value: "pressure", id: "EnumColorVariable" },
-        widgetType: "enum",
-        depth: 16,
-        ui: {
-          propType: "enum",
-          label: "Color variable",
-          domain: ["pressure",
-                   "uds_0_scalar",
-                   "axial_velocity",
-                   "radial_velocity",
-                   "tangential_velocity"
-                  ].reduce(function(res, cur) {
-            res[cur] = cur;
-            return res;
-          }, {}),
-          type: "string",
-          layout: '1',
-          help: "Which variable to color the contour or streamline?",
-          componentLabels: [''],
-        },
-        show: () => true,
-        onChange: function onChange(data) {
-          this.vizDialogSpec[data.id].data.value = data.value[0];
-          data.value = this.vizDialogSpec[data.id].data.value;
-          this.render();
-        }
-      },
-      // {
-      //   name: "contour value",
-      //   widgetType: "slider",
-      //   vals: [0.0, 800.0],
-      //   selected: [400.0],
-      //   id: "DoubleContourValue",
-      //   dataType: "double",
-      //   help: "Select a contour value",
-      // },
-      // {
-      //   name: "some other value",
-      //   widgetType: "cell",
-      //   vals: [0, 1],
-      //   selected: [0.5],
-      //   id: "CellValue",
-      //   dataType: "double",
-      //   help: "A little help text...",
-      // },
-      // {
-      //   name: "still another value",
-      //   widgetType: "cell",
-      //   vals: [0, 10],
-      //   selected: [5],
-      //   id: "CellValue2",
-      //   dataType: "int",
-      //   help: "A little help text...",
-      // },
-    };   
-
-    for (var key in this.vizDialogSpec) {
-      this.vizDialogSpec[key].onChange =
-        this.vizDialogSpec[key].onChange.bind(this);
-    };
-  }
-  
-  // The vizDialog is used to change or create a single entry in the
-  // vizCatalog.  This function is invoked by the vizDialog to park the new
-  // visualization in the catalog where it is convenient.
-  returnVizCatalogEntry() {
-
-    console.log("show state in returnVizCatalogEntry:", this.currentViz, this.state.vizCatalog, this.vizDialogSpec);
-
-    var newEntryName = this.vizDialogSpec.CellPlotName.data.value;
-    var newEntry = Object.values(this.vizDialogSpec).reduce(function(res, val) {
-      if (val.widgetType === "cell") {
-        res[val.data.id] = val.data.value[0];
-      } else {
-        res[val.data.id] = val.data.value;
-      };
-      return res;
-    }, {});
-        
-    // Reset the entry in the plot catalog that has just been modified.
-    var vizCatalogCopy = this.state.vizCatalog;
-    vizCatalogCopy[newEntryName] = newEntry;
-    this.setState({vizCatalog: vizCatalogCopy});
-
-    // Adjust the corresponding entry in the drawDialogSpec.  First we
-    // adjust the list of data sets to correspond to the data catalog (in
-    // case it has changed recently).
-    this.drawDialogSpec.plotName.ui.domain =
-      Object.keys(this.props.dataCatalog).reduce(function(res, cur) {
-        res[cur] = cur;
-        return res;
-      }, {});
-    // Then we adjust the list of available (named) visualizations.  We
-    // don't count on the setState() function above to have had effect yet,
-    // so we do it off the modified copy.
-    this.drawDialogSpec.dataSource.ui.domain =
-      Object.keys(vizCatalogCopy).reduce(function(res, cur) {
-        res[cur] = cur;
-        return res;
-      }, {});
-
-    console.log("return catalog entry", this.drawDialogSpec);
-    
-    // Draw this plot.  
-    this.props.executeDrawCommand({
-      visualization: this.vizDialogSpec.CellPlotName.data.value[0],
-      data: this.drawDialogSpec.dataSource.data.value,
-      vizCatalog: vizCatalogCopy
-    });
-
-    this.render();
-  }
 
   returnDrawCommand() {
     //console.log("returned draw command:", p);
@@ -360,43 +148,24 @@ class AMSControlPanel extends React.Component {
 
     this.render();
   }
-  
+
 
   render() {
 
     this.buildDrawDialogSpecs();
     //console.log("AMSControlPanel render: ", this.state);
 
-    this.vizDialogSpec.EnumContourVariable.ui.domain =
-      Object.keys(this.props.dataCatalog["m100rpm"].variables).reduce(
-        function(res, cur) {
-          res[cur] = cur;
-          return res;
-        }, {});
-
-    this.vizDialogSpec.EnumColorVariable.ui.domain =
-      Object.keys(this.props.dataCatalog["m100rpm"].variables).reduce(
-        function(res, cur) {
-          res[cur] = cur;
-          return res;
-        }, {});          
-    
     var testy = {hello: 52.6};
     //console.log("gto:", this.props.gto);
 
     return (
         <center>
         <div style={{width: '100%', display: 'table'}}>
-        <AMSPlotDialog buttonLabel="Edit plot descriptions"
-                       title="Edit plot description"
-                       dialogSpec={this.vizDialogSpec}
-                       closeLabel="Save"
-                       returnDialogResults={this.returnVizCatalogEntry}/>        
         <AMSPlotDialog buttonLabel="Choose plot to draw"
                        title="Select visualization"
                        closeLabel="Draw"
                        dialogSpec={this.drawDialogSpec}
-                       returnDialogResults={this.returnDrawCommand}/>        
+                       returnDialogResults={this.returnDrawCommand}/>
         <div style={{display: 'table-cell'}}>
         <button onClick={() => this.props.model.pvwClient.amsService.testButton(testy)}>test</button>
         <button onClick={() => this.props.model.pvwClient.amsService.showTankGeometry()}>tank</button>
--- js/src/AMSPlot/index.js
+++ js/src/AMSPlot/index.js
@@ -4,6 +4,7 @@ import SizeHelper from 'paraviewweb/src/Common/Misc/SizeHelper';
 import ParaViewWebClient from 'paraviewweb/src/IO/WebSocket/ParaViewWebClient';
 
 import AMSControlPanel from '../AMSControlPanel';
+import AMSPlotDialog from '../AMSPlotDialog';
 
 import SmartConnect from 'wslink/src/SmartConnect';
 
@@ -20,9 +21,9 @@ import SmartConnect from 'wslink/src/SmartConnect';
 // more level so there can multiples of this object while only having
 // one of those.
 //
-// 1. Move render canvas from above down to here.
-// 2. Move edit plot description from the control panel to this object.
-// 3. Move edit plot description from this object to its parent.
+// 1. Move render canvas from above down to here.  DONE
+// 2. Move edit plot description from the control panel to this object. DONE
+// 3. Move render canvas down one more level.
 
 
 class AMSPlot extends React.Component {
@@ -85,7 +86,7 @@ class AMSPlot extends React.Component {
       this.renderer = VtkRenderer.newInstance({ client: this.model.pvwClient });
 
       // Place it in the container set up for it.
-      this.renderer.setContainer(document.getElementById("canvasContainer"));
+      this.renderer.setContainer(document.getElementById("renderContainer"));
       // renderer.onImageReady(() => {
       //   console.log('image ready (for next command)');
       // });
@@ -114,7 +115,13 @@ class AMSPlot extends React.Component {
       };
     },1000);
 
+    this.vizDialogSpec = {};
+    this.currentViz = Object.keys(this.vizCatalog)[0];
+
     this.onDrawCommand = this.onDrawCommand.bind(this);
+
+    this.returnVizCatalogEntry = this.returnVizCatalogEntry.bind(this);
+    this.buildVizDialogSpecs();
   }
 
   onDrawCommand(drawCommand) {
@@ -122,23 +129,238 @@ class AMSPlot extends React.Component {
     this.model.pvwClient.amsService.executePlot(drawCommand);
   }
 
+  buildVizDialogSpecs() {
+
+    this.vizDialogSpec = {
+      CellPlotName: {
+        data: { value: [ this.currentViz ], id: "CellPlotName" },
+        widgetType: "cell",
+        depth: 1,
+        ui: {
+          propType: "cell",
+          label: "Visualization name",
+          domain: { range: [{ force: false }] },
+          type: "string",
+          layout: '1',
+          help: "Give this collection of plot parameters a name so you can use it again.",
+          componentLabels: [''],
+        },
+        show: () => true,
+        onChange: function onChange(data) {
+          console.log("changing the plot name:", data);
+          this.vizDialogSpec[data.id].data.value = data.value;
+          this.currentViz = data.value[0];
+          this.render();
+        }
+      },
+      EnumPlotType: {
+        data: { value: "contour", id: "EnumPlotType" },
+        widgetType: "enum",
+        depth: 4,
+        ui: {
+          propType: "enum",
+          label: "Visualization type",
+          domain: ["contour", "streamlines"].reduce(function(res, cur) {
+            res[cur] = cur;
+            return res;
+          }, {}),  // the list of possible values
+          type: "string",           // 'string' or 'int'
+          layout: '1',
+          help: "Choose the type of plot to view.",
+          componentLabels: [''],
+        },
+        show: () => true,
+        onChange: function onChange(data) {
+          this.vizDialogSpec[data.id].data.value = data.value[0];
+          data.value = this.vizDialogSpec[data.id].data.value;
+          this.render();
+        }
+      },
+      EnumContourVariable: {
+        data: { value: "uds_0_scalar", id: "EnumContourVariable" },
+        widgetType: "enum",
+        depth: 8,
+        ui: {
+          propType: "enum",
+          label: "Contour variable",
+          domain: ["uds_0_scalar",
+                   "pressure",
+                   "axial_velocity",
+                   "radial_velocity",
+                   "tangential_velocity"
+                  ].reduce(function(res, cur) {
+            res[cur] = cur;
+            return res;
+          }, {}),
+          type: "string",
+          layout: '1',
+          help: "Which value to contour?",
+          componentLabels: [''],
+        },
+        show: () => {
+          if (this.vizDialogSpec.EnumPlotType.data.value === "contour") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        onChange: function onChange(data) {
+          this.vizDialogSpec[data.id].data.value = data.value[0];
+          data.value = this.vizDialogSpec[data.id].data.value;
+          this.render();
+        }
+      },
+      DoubleContourValue: {
+        data: {value: [400.0], id: "DoubleContourValue" },
+        widgetType: "cell",
+        depth: 12,
+        ui: {
+          propType: "cell",
+          label: "Contour value",
+          domain: { range: [{ min: 0.0, max: 800.0, force: true }] },
+          type: "double",
+          layout: '1',
+          help: "Select a contour value",
+          componentLabels: [''],
+        },
+        show: () => {
+          if (this.vizDialogSpec.EnumPlotType.data.value === "contour") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        onChange: function onChange(data) {
+          this.vizDialogSpec[data.id].data.value = data.value;
+          this.render();
+        }
+      },
+      EnumColorVariable: {
+        data: { value: "pressure", id: "EnumColorVariable" },
+        widgetType: "enum",
+        depth: 16,
+        ui: {
+          propType: "enum",
+          label: "Color variable",
+          domain: ["pressure",
+                   "uds_0_scalar",
+                   "axial_velocity",
+                   "radial_velocity",
+                   "tangential_velocity"
+                  ].reduce(function(res, cur) {
+            res[cur] = cur;
+            return res;
+          }, {}),
+          type: "string",
+          layout: '1',
+          help: "Which variable to color the contour or streamline?",
+          componentLabels: [''],
+        },
+        show: () => true,
+        onChange: function onChange(data) {
+          this.vizDialogSpec[data.id].data.value = data.value[0];
+          data.value = this.vizDialogSpec[data.id].data.value;
+          this.render();
+        }
+      },
+      // {
+      //   name: "contour value",
+      //   widgetType: "slider",
+      //   vals: [0.0, 800.0],
+      //   selected: [400.0],
+      //   id: "DoubleContourValue",
+      //   dataType: "double",
+      //   help: "Select a contour value",
+      // },
+      // {
+      //   name: "some other value",
+      //   widgetType: "cell",
+      //   vals: [0, 1],
+      //   selected: [0.5],
+      //   id: "CellValue",
+      //   dataType: "double",
+      //   help: "A little help text...",
+      // },
+      // {
+      //   name: "still another value",
+      //   widgetType: "cell",
+      //   vals: [0, 10],
+      //   selected: [5],
+      //   id: "CellValue2",
+      //   dataType: "int",
+      //   help: "A little help text...",
+      // },
+    };
+
+    for (var key in this.vizDialogSpec) {
+      this.vizDialogSpec[key].onChange =
+        this.vizDialogSpec[key].onChange.bind(this);
+    };
+  }
+
+  // The vizDialog is used to change or create a single entry in the
+  // vizCatalog.  This function is invoked by the vizDialog to park the new
+  // visualization in the catalog where it is convenient.
+  returnVizCatalogEntry() {
+
+    console.log("show state in returnVizCatalogEntry:", this.currentViz, this.vizCatalog, this.vizDialogSpec);
+
+    var newEntryName = this.vizDialogSpec.CellPlotName.data.value;
+    var newEntry = Object.values(this.vizDialogSpec).reduce(function(res, val) {
+      if (val.widgetType === "cell") {
+        res[val.data.id] = val.data.value[0];
+      } else {
+        res[val.data.id] = val.data.value;
+      };
+      return res;
+    }, {});
+
+    // Reset the entry in the plot catalog that has just been modified.
+    this.vizCatalog[newEntryName] = newEntry;
+
+    this.render();
+  }
+
   render() {
     console.log("rendering AMSPlot:", this.model);
+
+    this.vizDialogSpec.EnumContourVariable.ui.domain =
+      Object.keys(this.props.dataCatalog["m100rpm"].variables).reduce(
+        function(res, cur) {
+          res[cur] = cur;
+          return res;
+        }, {});
+
+    this.vizDialogSpec.EnumColorVariable.ui.domain =
+      Object.keys(this.props.dataCatalog["m100rpm"].variables).reduce(
+        function(res, cur) {
+          res[cur] = cur;
+          return res;
+        }, {});
+
     return (
-      <div>
-        <AMSControlPanel model={this.model}
-                         vizCatalog={this.vizCatalog}
-                         dataCatalog={this.props.dataCatalog}
-                         executeDrawCommand={this.onDrawCommand}
+        <div>
+        <AMSPlotDialog buttonLabel="Edit plot descriptions"
+                       title="Edit plot descriptions"
+                       dialogSpec={this.vizDialogSpec}
+                       closeLabel="Save"
+                       returnDialogResults={this.returnVizCatalogEntry}
         />
-        <div id="canvasContainer"
-             style={{position: 'relative',
-                     width: '100vw',
-                     height: '100vh',
-                     overflow: 'hidden',
-                     zIndex: '10',
-                    }}
-        />
+        <div>
+          <AMSControlPanel model={this.model}
+                           vizCatalog={this.vizCatalog}
+                           dataCatalog={this.props.dataCatalog}
+                           executeDrawCommand={this.onDrawCommand}
+          />
+          <div id="renderContainer"
+               style={{position: 'relative',
+                       width: '100vw',
+                       height: '100vh',
+                       overflow: 'hidden',
+                       zIndex: '10',
+                      }}
+          />
+        </div>
       </div>
     )
   }
